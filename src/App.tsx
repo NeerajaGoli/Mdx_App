aedc5c61f1fa9913a86d490fb3f482e5ea491357
Neeraja <neeraja.goli@zemosolabs.com>
2020-05-19 12:17:20 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { importMDX } from 'mdx.macro';
+
+
 
 function App() {
+  const Content = lazy(() => importMDX('./content.mdx'))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Content />
+        </Suspense>
+      </div>
   );
 }
 
